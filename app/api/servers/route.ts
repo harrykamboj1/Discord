af9bd5e8a4d32d1cc6670c867b2c831d08c89e5a
Harnoor singh <singhharnoor116@gmail.com>
2024-04-25 09:55:23 +0530

    Server page added

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import { MemberRole } from "@prisma/client";
 
 export async function POST(req: Request) {
   try {
@@ -18,8 +19,16 @@ export async function POST(req: Request) {
         name,
         imageUrl,
         inviteCode: uuidv4(),
+        channels: {
+          create: [{ name: "general", profileId: profile.id }],
+        },
+        members: {
+          create: [{ profileId: profile.id, role: MemberRole.ADMIN }],
+        },
       },
     });
+
+    return NextResponse.json(server);
   } catch (err) {
     console.error("[SERVERS_POST]", err);
     return new NextResponse("Internal Server Error", { status: 500 });
