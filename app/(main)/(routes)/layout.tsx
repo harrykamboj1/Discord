148a4a2c54663acb6bb9a5c64f1853c658101070
Harnoor singh <singhharnoor116@gmail.com>
2024-04-26 15:13:50 +0530

    Minor

--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -3,6 +3,7 @@ const MainLayout = async ({ children }: { children: React.ReactNode }) => {
     <div className="h-full">
       <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
       <main></main>
+
       {children}
     </div>
   );
