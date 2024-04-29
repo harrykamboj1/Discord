30c40bcbae4c849a3915828257f29dafdd3333d0
Harnoor singh <singhharnoor116@gmail.com>
2024-04-29 22:53:40 +0530

    minor change

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,5 +1,6 @@
 import InitialModal from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
+
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
 
