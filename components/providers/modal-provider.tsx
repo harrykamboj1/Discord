5bc597f750024d36ab1d00cc7ab4cf0c6a1bf591
Harnoor singh <singhharnoor116@gmail.com>
2024-05-01 07:27:43 +0530

    Create Modal added

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { CreateServerModal } from "@/components/modals/create-modal-server";
+import { useEffect, useState } from "react";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <CreateServerModal />
+    </>
+  );
+};
