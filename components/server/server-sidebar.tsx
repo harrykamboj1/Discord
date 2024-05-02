aa73cb4c9b358c5c57057634add5cb3ca9e62e98
Harnoor singh <singhharnoor116@gmail.com>
2024-05-02 07:48:26 +0530

    Server Header Ui Done

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -53,7 +53,9 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
     return redirect("/");
   }
 
-  const role = server?.members.find((member) => member.id === profile.id)?.role;
+  const role = server?.members.find(
+    (member) => member.profileId === profile.id
+  )?.role;
 
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
