e9beb5bf590b09958328d891bab5534492dce22e
Harnoor singh <singhharnoor116@gmail.com>
2024-05-05 17:02:38 +0530

    Member  Settings

--- components/modals/edit-members.tsx
+++ components/modals/edit-members.tsx
@@ -6,24 +6,78 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
-import { useModal } from "@/hooks/use-modal-store";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
 
+import qs from "query-string";
+import { useModal } from "@/hooks/use-modal-store";
 import { useOrigin } from "@/hooks/use-origin";
 import { ServerWithMembersWithProfiles } from "@/type";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { ScrollArea } from "../ui/scroll-area";
+import { UserAvater } from "@/components/user-avatar";
+import {
+  Check,
+  Gavel,
+  Loader2,
+  MoreVertical,
+  Shield,
+  ShieldCheck,
+  ShieldQuestion,
+} from "lucide-react";
+import { useState } from "react";
+import { MemberRole } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldCheck className="h-4 w-4 ml-2 text-rose-500" />,
+};
 
 export const EditMembersModal = () => {
   const { onOpen, isOpen, onClose, type, data } = useModal();
-
+  const [loadingId, setLoadingId] = useState("");
+  const router = useRouter();
   const origin = useOrigin();
   const { server } = data as { server: ServerWithMembersWithProfiles };
 
   const isModalOpen = isOpen && type === "members";
 
+  const onRoleChange = async (memberId: string, role: MemberRole) => {
+    try {
+      setLoadingId(memberId);
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id,
+          memberId,
+        },
+      });
+
+      const response = await axios.patch(url, { role });
+      router.refresh();
+      onOpen("members", { server: response.data });
+    } catch (e) {
+      console.log("Error during role change:" + e);
+    } finally {
+      setLoadingId("");
+    }
+  };
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black  overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
             Manage Members
@@ -35,8 +89,64 @@ export const EditMembersModal = () => {
         </DialogHeader>
         <ScrollArea className="mt-8 max-h-[420px] pr-6">
           {server?.members.map((member) => (
-            <div className="flex items-center gap-x-2 mb-6" key={member.id}>
-              <UserAvater />
+            <div className="flex  items-center gap-x-2 mb-6" key={member.id}>
+              <UserAvater src={member?.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="text-xs font-semibold flex items-center gap-x-1">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
+              </div>
+              {server.profileId !== member.id && loadingId !== member.id && (
+                <div className="ml-auto">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <MoreVertical className="h-4 w-4 text-zinc-500" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent side="left">
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                          <ShieldQuestion className="w-4 h-4 mr-2" />
+                          <span>Role</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent>
+                            <DropdownMenuItem
+                              onClick={() => onRoleChange(member.id, "GUEST")}
+                            >
+                              <Shield className="w-4 h-4 mr-2" />
+                              Guest
+                              {member.role === "GUEST" && (
+                                <Check className="w-4 h-4 mr-2" />
+                              )}
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() =>
+                                onRoleChange(member.id, "MODERATOR")
+                              }
+                            >
+                              <ShieldCheck className="w-4 h-4 mr-2" />
+                              Moderator
+                              {member.role === "MODERATOR" && (
+                                <Check className="w-4 h-4 mr-2" />
+                              )}
+                            </DropdownMenuItem>
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>
+                        <Gavel className="h-4 w-4 mr-2" />
+                        Kick
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              )}
+              {loadingId === member.id && (
+                <Loader2 className="animate-spin text-zinc-500 ml-auto w-4 h-4" />
+              )}
             </div>
           ))}
         </ScrollArea>
