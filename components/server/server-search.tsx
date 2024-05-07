49066e061332ee47d855209ff5a35c54abf2a73b
Harnoor singh <singhharnoor116@gmail.com>
2024-05-07 08:41:49 +0530

    Search Bar changes

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { Search } from "lucide-react";
+import { CommandDialog, CommandInput } from "../ui/command";
+import { useState } from "react";
+import { CommandEmpty, CommandList } from "cmdk";
+
 interface ServerSearchProps {
   data:
     | {
@@ -16,5 +21,32 @@ interface ServerSearchProps {
     | undefined;
 }
 export const ServerSearch = ({ data }: ServerSearchProps) => {
-  return <div>Searver Search</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="group px-2 py-2 rounded-md flex items-center 
+      gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 
+      transition"
+      >
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+        <p
+          className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600
+        dark:group-hover:text-zinc-300 transition"
+        >
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">cmd</span>
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members" />
+        <CommandList>
+          <CommandEmpty>No Results Found</CommandEmpty>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
 };
