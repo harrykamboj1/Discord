13b85d804f43423f3b30a98844aa75483f8ab96c
Harnoor singh <singhharnoor116@gmail.com>
2024-05-07 07:29:49 +0530

    Leave Server Done

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -2,36 +2,70 @@
 import {
   Dialog,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 
 import { useModal } from "@/hooks/use-modal-store";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Check, Copy, RefreshCw } from "lucide-react";
-import { useOrigin } from "@/hooks/use-origin";
 import { useState } from "react";
 import axios from "axios";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { useRouter } from "next/navigation";
 
 export const LeaveServerModal = () => {
-  const { onOpen, isOpen, onClose, type, data } = useModal();
-  const [copied, setCopied] = useState(false);
+  const { isOpen, onClose, type, data } = useModal();
   const [isLoading, setIsLoading] = useState(false);
   const { server } = data;
-  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
   const isModalOpen = isOpen && type === "invite";
+  const router = useRouter();
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/servers/${server?.id}/leave`);
+
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Invite Friends
+            Leave Server
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to leave{" "}
+            <span className="text-indigo-500">{server?.name}</span>?
+          </DialogDescription>
         </DialogHeader>
-        <div className="p-6">Leave server</div>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isLoading}
+              onClick={() => onClose}
+              variant="ghost"
+            >
+              Cancel
+            </Button>
+            <Button
+              disabled={isLoading}
+              variant="primary"
+              onClick={() => onClick}
+            >
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
