2f4f16f5fb682b979059671133f36621a87be2eb
Harnoor singh <singhharnoor116@gmail.com>
2024-05-10 08:47:46 +0530

    Edit channel

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,6 +8,8 @@ import { EditMembersModal } from "../modals/edit-members";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "../modals/leave-server-modal";
 import { DeleteServerModal } from "../modals/delete-server-modal";
+import { DeleteChannelModal } from "../modals/delete-channel-modal";
+import { EditChannelModal } from "../modals/edit-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -29,6 +31,8 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
