87a359aa949cd5e1d5db36242d817154b5a18dc3
Harnoor singh <singhharnoor116@gmail.com>
2024-05-11 18:16:54 +0530

    Mobile View Added

---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,24 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
+import ServerSidebar from "./server/server-sidebar";
+const MobileToggle = ({ serverId }: { serverId?: string }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="p-0 flex gap-0" side="left">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId ?? ""} />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileToggle;
