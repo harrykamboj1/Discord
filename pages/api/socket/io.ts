ce92706576bdc21ee06d6b2213d6730512b4d292
Harnoor singh <singhharnoor116@gmail.com>
2024-05-13 06:49:14 +0530

    Socket.io

---
+++ pages/api/socket/io.ts
@@ -0,0 +1,7 @@
+import { NextApiResponseServerIo } from "@/type";
+import { Server as NetServer } from "net";
+import { NextApiRequest } from "next";
+
+import { Server as ServerIO } from "socket.io";
+
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {};
