79558dea42960bc32131da48b89e827331804e0a
Harnoor singh <singhharnoor116@gmail.com>
2024-05-13 08:39:09 +0530

    Socket.io Connection established

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,7 +1,25 @@
 import { NextApiResponseServerIo } from "@/type";
-import { Server as NetServer } from "net";
 import { NextApiRequest } from "next";
-
+import { Server as NetServer } from "http";
 import { Server as ServerIO } from "socket.io";
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+  if (!res.socket.server.io) {
+    const path = "/api/socket/io";
+    const httpServer: NetServer = res.socket.server as any;
+    const io = new ServerIO(httpServer, {
+      path: path,
+      addTrailingSlash: false,
+    });
 
-const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {};
+    res.socket.server.io = io;
+  }
+
+  res.end();
+};
+
+export default ioHandler;
