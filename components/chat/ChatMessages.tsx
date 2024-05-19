01fb479fe29cc950ee6f5726a5edd3c11ff85e5a
Harnoor singh <singhharnoor116@gmail.com>
2024-05-19 17:52:20 +0530

    Messages Done

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -4,7 +4,7 @@ import { Member, Message, Profile } from "@prisma/client";
 import ChatWelcome from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
-import { Fragment } from "react";
+import { ElementRef, Fragment, useRef } from "react";
 import { ChatItem } from "./chat-item";
 import { format } from "date-fns";
 import { useChatSocket } from "@/hooks/use-chat-socket";
@@ -42,6 +42,10 @@ const ChatMessage = ({
   const queryKey = `chat:${chatId}`;
   const addKey = `chat:${chatId}:messages`;
   const updateKey = `chat:${chatId}:messages:update`;
+
+  const chatRef = useRef<ElementRef<"div">>(null);
+  const bottomRef = useRef<ElementRef<"div">>(null);
+
   const { data, hasNextPage, fetchNextPage, isFetchingNextPage, status } =
     useChatQuery({ queryKey, apiUrl, paramKey, paramValue });
   useChatSocket({ queryKey, addKey, updateKey });
@@ -68,10 +72,25 @@ const ChatMessage = ({
     );
   }
 
+  console.log(isFetchingNextPage);
   return (
-    <div className="flex flex-1 flex-col py-4 overflow-y-auto">
-      <div className="flex-1" />
-      <ChatWelcome type={type} name={name} />
+    <div ref={chatRef} className="flex flex-1 flex-col py-4 overflow-y-auto">
+      {!hasNextPage && <div className="flex-1" />}
+      {!hasNextPage && <ChatWelcome type={type} name={name} />}
+      {hasNextPage && (
+        <div className="flex justify-center">
+          {isFetchingNextPage ? (
+            <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4" />
+          ) : (
+            <button
+              onClick={() => fetchNextPage()}
+              className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 dark:hover:text-zinc-300 transition"
+            >
+              Load Previous Messages...
+            </button>
+          )}
+        </div>
+      )}
       <div className="flex flex-col-reverse mt-auto">
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
@@ -93,6 +112,7 @@ const ChatMessage = ({
           </Fragment>
         ))}
       </div>
+      <div ref={bottomRef} />
     </div>
   );
 };
---
+++ pages/api/socket/direct-messages copy/index.ts
@@ -0,0 +1,99 @@
+import { currentProfile } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
+import { NextApiResponseServerIo } from "@/type";
+import { NextApiRequest } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    console.log("ENTER ***************");
+    const profile = await currentProfile(req);
+    const { content, fileUrl } = req.body;
+    const { conversationId } = req.query;
+
+    if (!profile) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if (!conversationId) {
+      return res.status(401).json({ error: "conversationId Id Missing" });
+    }
+    if (!content) {
+      return res.status(401).json({ error: "Content Missing" });
+    }
+
+    const conversation = await db.conversation.findFirst({
+      where: {
+        id: conversationId as string,
+        OR: [
+          {
+            memberOne: {
+              profileId: profile.id,
+            },
+          },
+          {
+            memberTwo: {
+              profileId: profile.id,
+            },
+          },
+        ],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    if (!conversation) {
+      return res.status(404).json({ message: "Conversation not found" });
+    }
+
+    const member =
+      conversation.memberOne.profileId === profile.id
+        ? conversation.memberOne
+        : conversation.memberTwo;
+
+    if (!member) {
+      return res.status(404).json({ error: "Member not found" });
+    }
+
+    const message = await db.directMessage.create({
+      data: {
+        content,
+        fileUrl,
+        conversationId: conversationId as string,
+        memberId: member.id,
+      },
+      include: {
+        member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+    console.log("Message ***************");
+    const channelKey = `chat:${conversationId}:messages`;
+
+    res?.socket.server?.io.emit(channelKey, message);
+
+    return res.status(200).json(message);
+  } catch (e) {
+    console.log("DIRECT_MESSAGE_POST", e);
+    res.status(500).json({ error: "Internal Error" });
+  }
+}
